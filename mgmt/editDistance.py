d12e094b748d87ab540c4c2037ce8571c5d0d8ff
TheCoolestTaco <eduardogonzalez@Eduardos-MacBook-Pro.local>
2016-11-15 13:05:29 -0600

    finished combinations

--- mgmt/editDistance.py
+++ mgmt/editDistance.py
@@ -45,78 +45,178 @@ def levenshtein(sentOne, sentTwo):
 
     return editMatrix[len(sentTwo) - 1][len(sentOne) - 1]
 
+def mainFunction (numOfLoops, countryOne, countryTwo):
 
-
-if __name__ == '__main__':
-    editDistance = 10
+    editDistance = 5
     
     #emoji code
     try:
-    # Wide UCS-4 build
+        #Wide UCS-4 build
         emoji_pattern = re.compile(u'['
-                      u'\U0001F300-\U0001F64F'
-                      u'\U0001F680-\U0001F6FF'
-                      u'\u2600-\u26FF\u2700-\u27BF]+',
-                      re.UNICODE)
+                                   u'\U0001F300-\U0001F64F'
+                                   u'\U0001F680-\U0001F6FF'
+                                   u'\u2600-\u26FF\u2700-\u27BF]+',
+                                   re.UNICODE)
     except re.error:
-    # Narrow UCS-2 build
+        #Narrow UCS-2 build
         emoji_pattern = re.compile(u'('
-                      u'\ud83c[\udf00-\udfff]|'
-                      u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
-                      u'[\u2600-\u26FF\u2700-\u27BF]|'
-                      u'\ud83d[\u25A0-\u25FF])+',
-                      re.UNICODE)
-    tweets = []
-
-    #importing all the tweets
-    with open('data/abc_es_tweets.csv', 'rb') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            #ignore the first line
-            if (row[0] == "text"):
-                continue
-            teststring = unicode(row[0], 'utf-8')
-            teststring = emoji_pattern.sub(r'', teststring)
-            
-            row[0] = teststring
-            #print row[0]
-            tweets.append(row[0])
-            #print "----------------------"
-
-
-    currentT = ""
-    tempT = ""
-
-    currentTWords = []
-    tempTWords = []
-
-    #beginning of edit distance
-    for i in range(0, len(tweets) -1):
-        #words are now stored in here
-        currentT = tweets[i]
-        currentTWords = currentT.split()
+                                   u'\ud83c[\udf00-\udfff]|'
+                                   u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
+                                   u'[\u2600-\u26FF\u2700-\u27BF]|'
+                                   u'\ud83d[\u25A0-\u25FF])+',
+                                   re.UNICODE)
+    
+    
+    
+    for i in range(numOfLoops):
+        tweetsOne = []
+        tweetsTwo = []
         
-        #tweet comparing
-        for j in range(0, len(tweets) -1):
-            if i == j:
-                continue
-            else:
-                tempT = tweets[j]
-                tempTWords = tempT.split()
+        fileOne = 'data/' + countryOne[i] + '_tweets' + '.csv'
+        fileTwo = 'data/' + countryTwo[i+1] + '_tweets' + '.csv'
+        
+        #debugging purposes
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        print(fileOne + " " + fileTwo)
+        print(i)
+        
+        
+        #importing all the tweets for acocunt 1
+        with open(fileOne, 'rb') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                #ignore the first line
+                if (row[0] == "text" or row[0] == " " or row[0] == ""):
+                    continue
+                teststring = unicode(row[0], 'utf-8')
+                teststring = emoji_pattern.sub(r'', teststring)
                 
+                row[0] = teststring
+                tweetsOne.append(row[0])
+    
+        #importing all the tweets for acocunt 2
+        with open(fileTwo, 'rb') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                #ignore the first line
+                if (row[0] == "text" or row[0] == " " or row[0] == ""):
+                    continue
+                teststring = unicode(row[0], 'utf-8')
+                teststring = emoji_pattern.sub(r'', teststring)
                 
-                #at this point we have both words and both sets of words, now we find the edit distance
-                distance = levenshtein(currentTWords, tempTWords)
+                row[0] = teststring
+                tweetsTwo.append(row[0])
 
-                if distance <= editDistance:
-                    print ("done")
-                    print("--------------------------------------")
-                    print("Current")
-                    print(currentTWords)
-                    print("------------------")
-                    print("Comparing")
-                    print(tempTWords)
-                    print("--------------------------------------")
-                    print("++++++++++++++++++++++++++++++++++++++++++")
-                    print(distance)
-                    print("++++++++++++++++++++++++++++++++++++++++++")
+        currentT = ""
+        tempT = ""
+        
+        currentTWords = []
+        tempTWords = []
+        
+        #beginning of edit distance
+        for i in range(0, len(tweetsOne) -1):
+            #words are now stored in here
+            currentT = tweetsOne[i]
+            currentTWords = currentT.split()
+            
+            #tweet comparing
+            for j in range(0, len(tweetsTwo) -1):
+                if i == j:
+                    continue
+                else:
+                    tempT = tweetsTwo[j]
+                    tempTWords = tempT.split()
+                    
+                    #at this point we have both words and both sets of words, now we find the edit distance
+                    distance = levenshtein(currentTWords, tempTWords)
+                    
+                    if distance <= editDistance:
+                        print ("done")
+                        print("--------------------------------------")
+                        print("Current")
+                        print(currentTWords)
+                        print("------------------")
+                        print("Comparing")
+                        print(tempTWords)
+                        print("--------------------------------------")
+                        print("++++++++++++++++++++++++++++++++++++++++++")
+                        print(distance)
+                        print("++++++++++++++++++++++++++++++++++++++++++")
+
+
+
+
+
+if __name__ == '__main__':
+    chile 	=	["latercera", "Emol", "TVN", "CNNChile", "Cooperativa"]
+    arg		=	["eltreceoficial", "LANACION", "clarincom", "24conurbano", "populardiario"]
+    col 	=	["NoticiasRCN", "ELTIEMPO", "elespectador", "elpaiscali"]
+    mex		=	["CNNMex", "NTelevisa_com", "lopezdoriga", "EPN"]
+    spain	=	["abc_es", "informacion_es", "elperiodico", "_rebelion_org", "Overwatch_Esp", "radiocable"]
+    usa 	=	["ElNuevoDia", "primerahora", "LaOpinionLA", "elnuevoherald", "vivelohoy", "despiertamerica"]
+
+    #CHILE - SELF
+    mainFunction(4, chile, chile)
+
+    #ARGENTINA - SELF
+    mainFunction(4, arg, arg)
+
+    #COLUMBIA - SELF
+    mainFunction(3, col, col)
+
+    #MEXICO - SELF
+    mainFunction(3, mex, mex)
+
+    #SPAIN - SELF
+    mainFunction(5, spain, spain)
+
+    #USA - SELF
+    mainFunction(5, usa, usa)
+
+    #CHILE - ARGENTINA
+    mainFunction(4, chile, arg)
+
+    #CHILE - COLUMBIA
+    mainFunction(4, col, chile)
+
+    #CHILE - MEXICO
+    mainFunction(4, mex, chile)
+
+    #CHILE - SPAIN
+    mainFunction(5, chile, spain)
+
+    #CHILE - USA
+    mainFunction(5, chile, usa)
+
+    #ARGENTINA - COLUMBIA
+    mainFunction(4, col, arg)
+
+    #ARGENTINA - MEXICO
+    mainFunction(4, mex, arg)
+
+    #ARGENTINA - SPAIN
+    mainFunction(5, arg, spain)
+
+    #ARGENTINA - USA
+    mainFunction(5, arg, usa)
+
+    #COLUMBIA - MEXICO
+    mainFunction(3, col, mex)
+
+    #COLUMBIA - SPAIN
+    mainFunction(4, col, spain)
+
+    #COLUMBIA - USA
+    mainFunction(4, col, usa)
+
+    #MEXICO - SPAIN
+    mainFunction(4, mex, spain)
+
+    #MEXICO - USA
+    mainFunction(4, mex, usa)
+
+    #SPAIN - USA
+    mainFunction(5, usa, spain)
+
+
+
