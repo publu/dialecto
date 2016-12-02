8d2ac6218492eedae833d09fa132d7d45a5a23ac
dmo140230 <dorozco6994@gmail.com>
2016-12-02 14:46:48 -0600

    Add files via upload
    tweet_combiner.py merges tweets from the same country to fill up the 1024 character limit and use less rows
    c_results is the csv containing the merged tweets from the results.csv file created by the unigram filter

---
+++ unigram/results/tweet_combiner.py
@@ -0,0 +1,56 @@
+import csv
+
+def add_string_c(tweet, key):
+    for i in range(len(d[key])):
+        #print d[key][i]
+        l = len(tweet) + len(d[key][i])
+        #print l
+        if l < 1024:
+            s = d[key][i] + tweet
+            #print s
+            d[key][i] = s
+            return True
+    return False
+            
+def save_combined_tweets(tweet_dict):
+    outtweets = []
+    with open('c_results.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        #For every country
+        for key in tweet_dict.keys():
+            #For every combined tweet in that country
+            for ctweet in tweet_dict[key]:
+                #Format the output: [[tweet1, countrya], [tweet2, countrya], [tweet1, countryb]]
+                outtweets.append([ctweet, key])
+        writer.writerows(outtweets)
+        
+        
+#Open the results file
+#countries = set()
+d = {}
+tweets = []
+countries = []
+print "Now opening file..."
+with open('results.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        tweet = row[0]
+        c = row[1]
+        tweets.append(tweet)
+        countries.append(c)
+        d[c] = []
+print "Done opening file\n"  
+total = len(tweets)
+j = 0.0
+print "Now merging..."
+for i in range(len(tweets)):
+    tweet = tweets[i]
+    c = countries[i]
+    if not add_string_c(tweet, c):
+            d[c].append(tweet)
+    j += 1
+    p = round((j/total * 100), 4)
+    print "Percent complete: " + str(p)
+            
+save_combined_tweets(d)
+            
